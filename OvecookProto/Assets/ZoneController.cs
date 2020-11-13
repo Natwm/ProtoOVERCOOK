633b8a3f86454769e1cf9015c569740550ec36c8
Pierre <pierre.rellic@gmail.com>
2020-11-13 15:29:22 +0100

    Add ZoneController and Pierre Scene

---
+++ OvecookProto/Assets/ZoneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneController : MonoBehaviour
+{
+    public Transform dropPosition;
+    private Gameobject _droppable;
+
+    public void OnDrop(GameObject droppable){
+        if(this._droppable == null)
+        {
+            _droppable = droppable;
+            _droppable.transform.position = dropPosition;
+        }
+    }
+
+    public virtual void Use() {}
+}
